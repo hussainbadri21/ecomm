de7307104be32730f139d51aaf6bb428bd5f0ed2
Hussain Calcuttawala <hussain.c@peopleinteractive.in>
2024-03-25 16:04:12 +0530

    Fixes css

--- src/app/_components/Fields/Password/index.tsx
+++ src/app/_components/Fields/Password/index.tsx
@@ -6,7 +6,7 @@ const PasswordField = ({ formData, setFormData }: { formData: Record<string, str
         <>
             <label className='text-black pt-8' htmlFor="password">Password</label>
             <div className='flex flex-row'>
-                <input className="p-2 rounded" type={showPassword ? "text" : "password"} id="password" name="password" placeholder="Enter a password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
+                <input className="p-2 rounded  w-full" type={showPassword ? "text" : "password"} id="password" name="password" placeholder="Enter a password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} />
                 <button className='p-2 rounded cursor-pointer bg-white underline text-gray-500 hover:text-gray-700' onClick={(e) => { e.preventDefault(); setShowPassword(!showPassword) }}>Show</button>
             </div>
         </>
